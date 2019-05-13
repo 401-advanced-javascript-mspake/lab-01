bab083a41b8fcce14fc8c144c212b0f55d2be0ba
Morgana Spake <morgana.spake@gmail.com>
2019-05-13 15:54:39 -0700

    added multiply and divide functions

--- __tests__/arithmetic.test.js
+++ __tests__/arithmetic.test.js
@@ -1,4 +1,3 @@
 'use strict';
 
 const arithmetic = require('../lib/arithmetic.js');
-
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 'use strict';
 
-const greet = require('./lib/greet.js');
+// const greet = require('./lib/greet.js');
 const math = require('./lib/arithmetic.js');
-console.log(greet('JOHN'));
+// console.log(greet('JOHN'));
 console.log(math.add(1,3)); // 4
 console.log(math.subtract(1,3)); // -2
+console.log(math.multiply(5,5)); // 25
+console.log(math.divide(10,2)); // 5
