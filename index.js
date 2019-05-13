e4cc410fb2ef48ff0c2bcf4b9af4993712e46c3c
Morgana Spake <morgana.spake@gmail.com>
2019-05-13 16:53:25 -0700

    greet module

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict';
 
-// const greet = require('./lib/greet.js');
+const greet = require('./lib/greet.js');
 const math = require('./lib/arithmetic.js');
-// console.log(greet('JOHN'));
+console.log(greet('JOHN')); // Hello, JOHN
+console.log(greet(12)); // null
 console.log(math.add([1,3,10])); // 14
 console.log(math.subtract([1,3,10])); // -12
 console.log(math.multiply([5,5,10])); // 250
 console.log(math.divide([10,2,5])); // 5
 console.log(math.divide([10,0])); // Cannot divide by zero
-
